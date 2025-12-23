8d8761939ceb9fecbaaef0b93b9a43f8ce012f4d
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-12-23 22:51:43 +0530

    added mini-project 1

---
+++ 05_mini-project-nextjs/components/footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-50 border-t border-gray-200 flex items-center justify-center">
+      <div className="max-w-6xl mx-auto px-4 py-8">
+        <p className="text-gray-600 text-sm">
+          2025 MyWebsite All Rights Reserved.
+        </p>
+        <p className="text-gray-500 text-xs mt-2 text-center">
+          Built with nextjs and Tailwindcss
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

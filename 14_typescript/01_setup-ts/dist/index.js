97fd8b6e5d9f1b94c3d920ac0e20e5e577599a1b
Himanshu Ramteke <himanshuramteke007@gmail.com>
2026-02-02 21:48:02 +0530

    added typesInts

--- 14_typescript/01_setup-ts/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello my name is ${name}, Welcome to TS setup.`;
-}
-const username = "Chai aur TS";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
---
+++ 14_typescript/chai-aur-typescript/src/typesInts.ts
@@ -0,0 +1,9 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "Chaiaurcode";
+
+let chaiFlavour = "Ginger Tea";
+
+let chaiOrder: boolean = true;
